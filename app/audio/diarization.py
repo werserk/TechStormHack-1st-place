283f565ebd681f8228e8a422c8bb75a71fcaf217
werserk <werserk@inbox.ru>
2024-08-23 21:40:29 +0300

    pyannote WORKING

--- app/audio/diarization.py
+++ app/audio/diarization.py
@@ -7,9 +7,12 @@ class SpeakerClassifier:
             "pyannote/speaker-diarization-3.1",
             use_auth_token="YOUR_HF_TOKEN_HERE")
 
-    def __call__(self, audio_file: str) -> None:
-        diarization = self.model("../data/audio/test.wav")
+    def __call__(self, audio_data) -> None:
+        return self.model(audio_data)
+
+    @staticmethod
+    def print_diarization(diarization) -> None:
         for segment, _, speaker in diarization.itertracks(yield_label=True):
             start_time = segment.start
             end_time = segment.end
-            print(f"Speaker {speaker} speaks from {start_time:.1f}s to {end_time:.1f}s")
+            print(f"{start_time:.1f}s-{end_time:.1f}s: {speaker}")
